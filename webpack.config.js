c4cb99322632b79d7140a196181b0c2c861ff3f4
Przemek Saja <przemek.saja@gmail.com>
2018-08-07 21:39:08 +0200

    Add configuration files

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const env = process.env.NODE_ENV || 'development';
+
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeJsPlugin = require('optimize-js-plugin');
+
+const plugins= [
+    new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        filename: 'index.html',
+        inject: 'body',
+    })
+];
+
+console.log('NODE_ENV:', env);
+
+if (env === 'production') {
+    plugins.push(
+        new webpack.optimize.UglifyJsPlugin(),
+        new OptimizeJsPlugin({
+            sourceMap: false
+        })
+    );
+};  
+
+
+//webpack.config.js
+module.exports = {
+    entry: [
+            'react-hot-loader/patch',
+            './src/index.js'
+            ],
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'app.bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                include: /src/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.css$/,
+                include: /css/,
+                use: [
+                    { 
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    }
+                ]
+            }
+
+        ]
+    },
+    plugins: plugins
+};
